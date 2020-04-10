c6864c6b99de37ccb52a59e818701c74ac8dfbe1
fairulazmin <fairulazmin@yahoo.com>
2020-04-11 05:32:03 +0800

    first commit

---
+++ home/javascript/non_unique_element.js
@@ -0,0 +1,24 @@
+
+function nonUniqueElements(data) {
+    var arr = data.slice();
+    var arr2 = data.sort((a,b) => a-b);
+    return arr2;
+}
+
+console.log(nonUniqueElements([1, 20, 3, 1, 3]))
+
+// "use strict";
+//
+// function nonUniqueElements(data) {
+//     return data;
+// }
+//
+// var assert = require('assert');
+//
+// if (!global.is_checking) {
+//     assert.deepEqual(nonUniqueElements([1, 2, 3, 1, 3]), [1, 3, 1, 3], "1st example");
+//     assert.deepEqual(nonUniqueElements([1, 2, 3, 4, 5]), [], "2nd example");
+//     assert.deepEqual(nonUniqueElements([5, 5, 5, 5, 5]), [5, 5, 5, 5, 5], "3rd example");
+//     assert.deepEqual(nonUniqueElements([10, 9, 10, 10, 9, 8]), [10, 9, 10, 10, 9], "4th example");
+//     console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
+// }
