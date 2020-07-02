8ea42f5a2ff8efa508be237d5e04281e2ed9ba2a
fairulazmin <fairulazmin@yahoo.com>
2020-07-02 21:21:05 +0800

    update

--- home/javascript/non_unique_element.js
+++ home/javascript/non_unique_element.js
@@ -1,24 +1,20 @@
 
 function nonUniqueElements(data) {
-    var arr = data.slice();
-    var arr2 = data.sort((a,b) => a-b);
-    return arr2;
+    let arr = []
+    for (let i=0; i<data.length; i++){
+        if (data.slice(i + 1).includes(data[i]) || arr.includes(data[i])) {
+            arr.push(data[i])
+        }
+    }
+    return arr
 }
 
-console.log(nonUniqueElements([1, 20, 3, 1, 3]))
+var assert = require('assert');
 
-// "use strict";
-//
-// function nonUniqueElements(data) {
-//     return data;
-// }
-//
-// var assert = require('assert');
-//
-// if (!global.is_checking) {
-//     assert.deepEqual(nonUniqueElements([1, 2, 3, 1, 3]), [1, 3, 1, 3], "1st example");
-//     assert.deepEqual(nonUniqueElements([1, 2, 3, 4, 5]), [], "2nd example");
-//     assert.deepEqual(nonUniqueElements([5, 5, 5, 5, 5]), [5, 5, 5, 5, 5], "3rd example");
-//     assert.deepEqual(nonUniqueElements([10, 9, 10, 10, 9, 8]), [10, 9, 10, 10, 9], "4th example");
-//     console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
-// }
+if (!global.is_checking) {
+    assert.deepEqual(nonUniqueElements([1, 2, 3, 1, 3]), [1, 3, 1, 3], "1st example");
+    assert.deepEqual(nonUniqueElements([1, 2, 3, 4, 5]), [], "2nd example");
+    assert.deepEqual(nonUniqueElements([5, 5, 5, 5, 5]), [5, 5, 5, 5, 5], "3rd example");
+    assert.deepEqual(nonUniqueElements([10, 9, 10, 10, 9, 8]), [10, 9, 10, 10, 9], "4th example");
+    console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
+}
